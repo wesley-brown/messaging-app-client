287f66e8b26edcb45d80268ee5baae6ccfbc39b5
Wesley Brown <wesleybrown@wesleybrown.net>
2020-03-02 12:33:40 -0500

    Display Message Content in Message Component
    A message component should display the content of the message it owns.

--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Message } from '../message';
+
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+  message: Message = { content: 'Hello, World!' };
 
   constructor() { }
 
--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MessageComponent } from './message/message.component';
 
 @NgModule({
